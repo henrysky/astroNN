318e19debd2590f5f7712996db575d719097b2d9
Jo Bovy <bovy@astro.utoronto.ca>
2017-11-01 15:00:54 -0400

    Make paths in setup.py general (for windows and linux)

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
+import os, os.path
 
 setup(
     name='astroNN',
     version='0.3',
-    packages=['astroNN', 'astroNN\\apogeetools','astroNN\\gaiatools','astroNN\\NN', 'astroNN\\datasets'],
+    packages=['astroNN', 
+              os.path.join('astroNN','apogeetools'),
+              os.path.join('astroNN','gaiatools'),
+              os.path.join('astroNN','NN'),
+              os.path.join('astroNN','datasets')],
     include_package_data=True,
     url='https://github.com/henrysky/astroNN/',
     license='MIT',
