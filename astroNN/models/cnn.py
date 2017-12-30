fix normalization issue

astroNN/models/cnn.py
@@ -91,10 +91,6 @@ class CNN(ModelStandard):
 
         csv_logger = CSVLogger(self.fullfilepath + 'log.csv', append=True, separator=',')
 
-        mean_labels = np.mean(y, axis=0)
-        std_labels = np.std(y, axis=0)
-        mu_std = np.vstack((mean_labels, std_labels))
-
         reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.5, epsilon=self.reduce_lr_epsilon,
                                       patience=self.reduce_lr_patience, min_lr=self.reduce_lr_min, mode='min',
                                       verbose=2)
@@ -111,8 +107,6 @@ class CNN(ModelStandard):
         astronn_model = 'model.h5'
         self.keras_model.save(self.fullfilepath + astronn_model)
         print(astronn_model + ' saved to {}'.format(self.fullfilepath + astronn_model))
-        np.save(self.fullfilepath + 'meanstd.npy', mu_std)
-        np.save(self.fullfilepath + 'targetname.npy', self.target)
 
         clear_session()
 
@@ -120,7 +114,11 @@ class CNN(ModelStandard):
 
     def test(self, x):
         x = super().test(x)
-        return self.keras_model.predict(x)
+        pred = self.keras_model.predict(x)
+        data = np.load(self.fullfilepath + '/meanstd.npy')
+        pred *= data[1]
+        pred += data[0]
+        return pred
 
 
 class DataGenerator(object):

astroNN/models/models_shared.py
@@ -202,13 +202,19 @@ class ModelStandard(ABC):
         if self.data_normalization is True:
             # do not include -9999 in mean and std calculation and do not normalize those elements because
             # astroNN is designed to ignore -9999. only
-            not9999 = np.where(y != -9999.)
-            mean_labels = np.median(y[not9999], axis=0)
-            std_labels = np.std(y[not9999], axis=0)
+            mean_labels = np.zeros(y.shape[1])
+            std_labels = np.ones(y.shape[1])
+            for i in range(y.shape[1]):
+                not9999 = np.where(y[:, i] != -9999.)[0]
+                print(not9999.shape)
+                mean_labels[i] = np.median((y[:, i])[not9999], axis=0)
+                std_labels[i] = np.std((y[:, i])[not9999], axis=0)
+                (y[:, i])[not9999] -= mean_labels[i]
+                (y[:, i])[not9999] /= std_labels[i]
+            print(mean_labels)
             mu_std = np.vstack((mean_labels, std_labels))
             np.save(self.fullfilepath + 'meanstd.npy', mu_std)
-
-            y[not9999] = (y[not9999] - mean_labels) / std_labels
+            np.save(self.fullfilepath + 'targetname.npy', self.target)
 
         self.input_shape = (x.shape[1], 1,)
         self.output_shape = (y.shape[1], 1,)
@@ -287,5 +293,5 @@ class ModelStandard(ABC):
 def target_conversion(target):
     if target == 'all' or target == ['all']:
         target = ['teff', 'logg', 'M', 'alpha', 'C', 'C1', 'N', 'O', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Ca', 'Ti',
-                  'Ti2', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Ge', 'Rb', 'Y', 'Nd', 'absmag']
+                  'Ti2', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'absmag']
     return np.asarray(target)

astroNN/models/starnet.py
@@ -119,7 +119,10 @@ class StarNet(ModelStandard):
 
     def test(self, x):
         x = super().test(x)
-        return self.keras_model.predict(x)
+        pred = self.keras_model.predict(x)
+        data = np.load(self.fullfilepath + '/meanstd.npy')
+        pred *= data[1]
+        pred += data[0]
 
 
 class DataGenerator(object):