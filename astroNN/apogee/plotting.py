fix jacobian performance issue

astroNN/apogee/plotting.py
@@ -4,6 +4,7 @@ import time
 import keras.backend as K
 
 from astroNN.models.NeuralNetMaster import NeuralNetMaster
+from astroNN import MAGIC_NUMBER
 
 K.set_learning_phase(1)
 
@@ -39,12 +40,10 @@ class ASPCAP_plots(NeuralNetMaster):
         if not os.path.exists(aspcap_residue_path):
             os.makedirs(aspcap_residue_path)
 
-        magic_number = -9999.
-
         std_labels = np.zeros(test_labels.shape[1])
 
         for i in range(test_labels.shape[1]):
-            not9999_index = np.where(test_labels[:, i] != magic_number)
+            not9999_index = np.where(test_labels[:, i] != MAGIC_NUMBER)
             std_labels[i] = np.std((test_labels[:, i])[not9999_index], axis=0)
 
         for i in range(self.labels_shape):
@@ -129,88 +128,93 @@ class ASPCAP_plots(NeuralNetMaster):
         input_tens = self.keras_model.layers[0].input
         jacobian = np.empty((self.labels_shape, x.shape[0], x.shape[1]), dtype=np.float16)
         start_time = time.time()
+        K.set_learning_phase(0)
 
         with K.get_session() as sess:
-            K.set_learning_phase(0)
             for counter, j in enumerate(range(self.labels_shape)):
-                print('Completed {} of {} output, {:.03f} seconds elapsed'.format(counter, self.labels_shape,
+                print('Completed {} of {} output, {:.03f} seconds elapsed'.format(counter + 1, self.labels_shape,
                                                                                   time.time() - start_time))
                 grad = self.keras_model.layers[-1].output[0, j]
+                grad_wrt_input_tensor = K.tf.gradients(grad, input_tens)
                 for i in range(x.shape[0]):
-                    jacobian[j, i:i + 1, :] = (np.asarray(sess.run(K.tf.gradients(grad, input_tens),
+                    jacobian[j, i:i + 1, :] = (np.asarray(sess.run(grad_wrt_input_tensor,
                                                                    feed_dict={input_tens: x[i:i + 1]})))[:, :, 0].T
 
-            # Some plotting variables for asthetics
-            plt.rcParams['axes.facecolor'] = 'white'
-            sns.set_style("ticks")
-            plt.rcParams['axes.grid'] = False
-            plt.rcParams['grid.color'] = 'gray'
-            plt.rcParams['grid.alpha'] = '0.4'
-            path = os.path.join(self.fullfilepath, 'jacobian')
-            if not os.path.exists(path):
-                os.makedirs(path)
+        K.clear_session()
 
-            fullname = self.targetname
-            lambda_blue, lambda_green, lambda_red = wavelength_solution(dr=dr)
+        jacobian = np.mean(jacobian, axis=0)
 
-            for j in range(self.labels_shape):
-                fig = plt.figure(figsize=(45, 30), dpi=150)
-                scale = np.max(np.abs((jacobian[j, :])))
-                scale_2 = np.min((jacobian[j, :]))
-                blue, green, red = chips_split(jacobian[j, :], dr=dr)
-                ax1 = fig.add_subplot(311)
-                fig.suptitle('{}, Average of {} Stars'.format(fullname[j], x.shape[0]), fontsize=50)
-                ax1.set_ylabel(r'$\partial$' + fullname[j], fontsize=40)
-                ax1.set_ylim(scale_2, scale)
-                ax1.plot(lambda_blue, blue, linewidth=0.9, label='astroNN')
-                ax2 = fig.add_subplot(312)
-                ax2.set_ylabel(r'$\partial$' + fullname[j], fontsize=40)
-                ax2.set_ylim(scale_2, scale)
-                ax2.plot(lambda_green, green, linewidth=0.9, label='astroNN')
-                ax3 = fig.add_subplot(313)
-                ax3.set_ylim(scale_2, scale)
-                ax3.set_ylabel(r'$\partial$' + fullname[j], fontsize=40)
-                ax3.plot(lambda_red, red, linewidth=0.9, label='astroNN')
-                ax3.set_xlabel(r'Wavelength (Angstrom)', fontsize=40)
+        # Some plotting variables for asthetics
+        plt.rcParams['axes.facecolor'] = 'white'
+        sns.set_style("ticks")
+        plt.rcParams['axes.grid'] = False
+        plt.rcParams['grid.color'] = 'gray'
+        plt.rcParams['grid.alpha'] = '0.4'
+        path = os.path.join(self.fullfilepath, 'jacobian')
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-                ax1.axhline(0, ls='--', c='k', lw=2)
-                ax2.axhline(0, ls='--', c='k', lw=2)
-                ax3.axhline(0, ls='--', c='k', lw=2)
+        fullname = self.targetname
+        lambda_blue, lambda_green, lambda_red = wavelength_solution(dr=dr)
 
-                try:
-                    if dr == 14:
-                        url = "https://svn.sdss.org/public/repo/apogee/idlwrap/trunk/lib/l31c/{}.mask".format(
-                            aspcap_windows_url_correction(self.targetname[j]))
-                    else:
-                        raise ValueError('Only support DR14')
-                    df = np.array(pd.read_csv(urlopen(url), header=None, sep='\t'))
-                    print(url)
-                    aspcap_windows = df * scale
-                    aspcap_blue, aspcap_green, aspcap_red = chips_split(aspcap_windows, dr=dr)
-                    ax1.plot(lambda_blue, aspcap_blue, linewidth=0.9, label='ASPCAP windows')
-                    ax2.plot(lambda_green, aspcap_green, linewidth=0.9, label='ASPCAP windows')
-                    ax3.plot(lambda_red, aspcap_red, linewidth=0.9, label='ASPCAP windows')
-                except:
-                    print('No ASPCAP windows data for {}'.format(aspcap_windows_url_correction(self.targetname[j])))
-                tick_spacing = 50
-                ax1.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
-                ax2.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing / 1.5))
-                ax3.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing / 1.7))
-                ax1.minorticks_on()
-                ax2.minorticks_on()
-                ax3.minorticks_on()
+        for j in range(self.labels_shape):
+            fig = plt.figure(figsize=(45, 30), dpi=150)
+            scale = np.max(np.abs((jacobian[j, :])))
+            scale_2 = np.min((jacobian[j, :]))
+            blue, green, red = chips_split(jacobian[j, :], dr=dr)
+            ax1 = fig.add_subplot(311)
+            fig.suptitle('{}, Average of {} Stars'.format(fullname[j], x.shape[0]), fontsize=50)
+            ax1.set_ylabel(r'$\partial$' + fullname[j], fontsize=40)
+            ax1.set_ylim(scale_2, scale)
+            ax1.plot(lambda_blue, blue, linewidth=0.9, label='astroNN')
+            ax2 = fig.add_subplot(312)
+            ax2.set_ylabel(r'$\partial$' + fullname[j], fontsize=40)
+            ax2.set_ylim(scale_2, scale)
+            ax2.plot(lambda_green, green, linewidth=0.9, label='astroNN')
+            ax3 = fig.add_subplot(313)
+            ax3.set_ylim(scale_2, scale)
+            ax3.set_ylabel(r'$\partial$' + fullname[j], fontsize=40)
+            ax3.plot(lambda_red, red, linewidth=0.9, label='astroNN')
+            ax3.set_xlabel(r'Wavelength (Angstrom)', fontsize=40)
 
-                ax1.tick_params(labelsize=30, width=2, length=20, which='major')
-                ax1.tick_params(width=2, length=10, which='minor')
-                ax2.tick_params(labelsize=30, width=2, length=20, which='major')
-                ax2.tick_params(width=2, length=10, which='minor')
-                ax3.tick_params(labelsize=30, width=2, length=20, which='major')
-                ax3.tick_params(width=2, length=10, which='minor')
-                ax1.legend(loc='best', fontsize=40)
-                plt.tight_layout()
-                plt.subplots_adjust(left=0.05)
-                plt.savefig(path + '/{}_jacobian.png'.format(self.targetname[j]))
-                plt.close('all')
-                plt.clf()
+            ax1.axhline(0, ls='--', c='k', lw=2)
+            ax2.axhline(0, ls='--', c='k', lw=2)
+            ax3.axhline(0, ls='--', c='k', lw=2)
+
+            try:
+                if dr == 14:
+                    url = "https://svn.sdss.org/public/repo/apogee/idlwrap/trunk/lib/l31c/{}.mask".format(
+                        aspcap_windows_url_correction(self.targetname[j]))
+                else:
+                    raise ValueError('Only support DR14')
+                df = np.array(pd.read_csv(urlopen(url), header=None, sep='\t'))
+                print(url)
+                aspcap_windows = df * scale
+                aspcap_blue, aspcap_green, aspcap_red = chips_split(aspcap_windows, dr=dr)
+                ax1.plot(lambda_blue, aspcap_blue, linewidth=0.9, label='ASPCAP windows')
+                ax2.plot(lambda_green, aspcap_green, linewidth=0.9, label='ASPCAP windows')
+                ax3.plot(lambda_red, aspcap_red, linewidth=0.9, label='ASPCAP windows')
+            except:
+                print('No ASPCAP windows data for {}'.format(aspcap_windows_url_correction(self.targetname[j])))
+            tick_spacing = 50
+            ax1.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+            ax2.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing / 1.5))
+            ax3.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing / 1.7))
+            ax1.minorticks_on()
+            ax2.minorticks_on()
+            ax3.minorticks_on()
+
+            ax1.tick_params(labelsize=30, width=2, length=20, which='major')
+            ax1.tick_params(width=2, length=10, which='minor')
+            ax2.tick_params(labelsize=30, width=2, length=20, which='major')
+            ax2.tick_params(width=2, length=10, which='minor')
+            ax3.tick_params(labelsize=30, width=2, length=20, which='major')
+            ax3.tick_params(width=2, length=10, which='minor')
+            ax1.legend(loc='best', fontsize=40)
+            plt.tight_layout()
+            plt.subplots_adjust(left=0.05)
+            plt.savefig(path + '/{}_jacobian.png'.format(self.targetname[j]))
+            plt.close('all')
+            plt.clf()
 
         return jacobian