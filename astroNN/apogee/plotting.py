attempt to fix jacobian calculation

astroNN/apogee/plotting.py
@@ -125,7 +125,7 @@ class ASPCAP_plots(NeuralNetMaster):
 
         x = np.atleast_3d(x)
         # enforce float16 because the precision doesnt really matter
-        input_tens = self.keras_model.layers[0].input
+        input_tens = self.keras_model.get_la
         jacobian = np.empty((self.labels_shape, x.shape[0], x.shape[1]), dtype=np.float16)
         start_time = time.time()
         K.set_learning_phase(0)
@@ -135,14 +135,13 @@ class ASPCAP_plots(NeuralNetMaster):
                 print('Completed {} of {} output, {:.03f} seconds elapsed'.format(counter + 1, self.labels_shape,
                                                                                   time.time() - start_time))
                 grad = self.keras_model.layers[-1].output[0, j]
-                grad_wrt_input_tensor = K.tf.gradients(grad, input_tens)
                 for i in range(x.shape[0]):
-                    jacobian[j, i:i + 1, :] = (np.asarray(sess.run(grad_wrt_input_tensor,
+                    jacobian[j, i:i + 1, :] = (np.asarray(sess.run(K.tf.gradients(grad, input_tens),
                                                                    feed_dict={input_tens: x[i:i + 1]})))[:, :, 0].T
 
         K.clear_session()
 
-        jacobian = np.mean(jacobian, axis=0)
+        jacobian = np.mean(jacobian, axis=1)
 
         # Some plotting variables for asthetics
         plt.rcParams['axes.facecolor'] = 'white'
@@ -162,6 +161,7 @@ class ASPCAP_plots(NeuralNetMaster):
             scale = np.max(np.abs((jacobian[j, :])))
             scale_2 = np.min((jacobian[j, :]))
             blue, green, red = chips_split(jacobian[j, :], dr=dr)
+            blue, green, red = blue[0], green[0], red[0]
             ax1 = fig.add_subplot(311)
             fig.suptitle('{}, Average of {} Stars'.format(fullname[j], x.shape[0]), fontsize=50)
             ax1.set_ylabel(r'$\partial$' + fullname[j], fontsize=40)
@@ -191,6 +191,7 @@ class ASPCAP_plots(NeuralNetMaster):
                 print(url)
                 aspcap_windows = df * scale
                 aspcap_blue, aspcap_green, aspcap_red = chips_split(aspcap_windows, dr=dr)
+                aspcap_blue, aspcap_green, aspcap_red = aspcap_blue[0], aspcap_green[0], aspcap_red[0]
                 ax1.plot(lambda_blue, aspcap_blue, linewidth=0.9, label='ASPCAP windows')
                 ax2.plot(lambda_green, aspcap_green, linewidth=0.9, label='ASPCAP windows')
                 ax3.plot(lambda_red, aspcap_red, linewidth=0.9, label='ASPCAP windows')

astroNN/models/NeuralNetMaster.py
@@ -154,7 +154,7 @@ class NeuralNetMaster(ABC):
             print('Skipped plot_model! graphviz and pydot_ng are required to plot the model architecture')
             pass
 
-    def jacobian(self, x=None, plotting=True):
+    def jacobian_1(self, x=None, plotting=True):
         """
         NAME: cal_jacobian
         PURPOSE: calculate jacobian
@@ -172,8 +172,8 @@ class NeuralNetMaster(ABC):
 
         x = np.atleast_3d(x)
         # enforce float16 because the precision doesnt really matter
-        input_tens = self.keras_model.layers[0].input
-        jacobian = np.empty((self.labels_shape, x.shape[0], x.shape[1]), dtype=np.float16)
+        input_tens = self.keras_model.get_layer("input").input
+        jacobian = np.ones((self.labels_shape, x.shape[1], x.shape[0]), dtype=np.float16)
         start_time = time.time()
         K.set_learning_phase(0)
 
@@ -181,10 +181,12 @@ class NeuralNetMaster(ABC):
             for counter, j in enumerate(range(self.labels_shape)):
                 print('Completed {} of {} output, {:.03f} seconds elapsed'.format(counter + 1, self.labels_shape,
                                                                                   time.time() - start_time))
-                grad = self.keras_model.layers[-1].output[0, j]
-                grad_wrt_input_tensor = K.tf.gradients(grad, input_tens)
+                grad = self.keras_model.get_layer("output").output[0, j]
                 for i in range(x.shape[0]):
-                    jacobian[j, i:i + 1, :] = (np.asarray(sess.run(grad_wrt_input_tensor,
-                                                                   feed_dict={input_tens: x[i:i + 1]})))[:, :, 0].T
+                    x_in = x[i:i + 1]
+                    jacobian[j, :, i:i + 1] = (np.asarray(sess.run(K.tf.gradients(grad, input_tens),
+                                                                   feed_dict={input_tens: x_in})[0]))
 
         K.clear_session()
+
+        return jacobian

astroNN/models/loss/regression.py
@@ -33,9 +33,9 @@ def mse_lin_wrapper(var, labels_err):
     def mse_lin(y_true, y_pred):
         # Neural Net is predicting log(var), so take exp, takes account the target variance, and take log back
         labels_err_x = K.tf.where(K.tf.equal(y_true, MAGIC_NUMBER), K.tf.zeros_like(y_true), labels_err)
-        y_pred_corrected = K.log(K.exp(y_pred) + labels_err_x)
+        y_pred_corrected = K.exp(y_pred) + labels_err_x
         wrapper_output = K.tf.where(K.tf.equal(y_true, MAGIC_NUMBER), K.tf.zeros_like(y_true),
-                                    0.5 * K.square(y_true - y_pred) * (K.exp(-y_pred_corrected)) + 0.5 * y_pred_corrected)
+                                    0.5 * K.square(y_true - y_pred) - 0.5* y_pred_corrected)
         return K.mean(wrapper_output, axis=-1)
 
     return mse_lin