f29d1c308b327b087cd4e26f6060cddaedee6f40
Henry Leung <henryskyleung@gmail.com>
2018-02-06 20:43:55 -0500

    try again again to fix genindex

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ import astroNN
 import sys
 import os
 
-sys.path.insert(0, os.path.dirname(os.getcwd()))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration ------------------------------------------------
 
