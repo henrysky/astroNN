094c3add4967d03e5cb09c90940f01d1b208c1dc
Henry Leung <henryskyleung@gmail.com>
2018-02-12 20:51:20 -0500

    try again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,11 +32,11 @@ sys.path.insert(0, os.path.abspath('../..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = ['nbsphinx',
+              'sphinx.ext.autodoc',
               'sphinx.ext.doctest',
               'sphinx.ext.mathjax',
-              'nbsphinx',
-              ]
+              'sphinx.ext.githubpages']
 
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
