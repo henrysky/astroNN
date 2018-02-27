214f0a38e31883453ab161d0558c8fc62339aa64
Henry Leung <henryskyleung@gmail.com>
2018-02-27 12:40:01 -0500

    add jacobian test case and explicitly run tf.global_variables_initializer()

--- test/apogee_model_test.py
+++ test/apogee_model_test.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from astroNN.models import Apogee_CNN, Apogee_BCNN
+from astroNN.models import Apogee_CNN, Apogee_BCNN, StarNet2017
 
 
 class ApogeeModelTestCase(unittest.TestCase):
@@ -16,17 +16,30 @@ class ApogeeModelTestCase(unittest.TestCase):
         neuralnet.max_epochs = 1
         neuralnet.train(random_xdata, random_ydata)
         prediction = neuralnet.test(random_xdata)
+        jacobian = neuralnet.jacobian(random_xdata)
 
         np.testing.assert_array_equal(prediction.shape, random_ydata.shape)
+        np.testing.assert_array_equal(jacobian.shape, [random_ydata.shape[1], random_xdata.shape[1], random_xdata.shape[0]])
 
         # Apogee_BCNN
         bneuralnet = Apogee_BCNN()
         bneuralnet.max_epochs = 1
         bneuralnet.train(random_xdata, random_ydata)
         prediction, prediction_err, model_err, predictive_err = bneuralnet.test(random_xdata)
+        jacobian = neuralnet.jacobian(random_xdata)
 
         np.testing.assert_array_equal(prediction.shape, random_ydata.shape)
+        np.testing.assert_array_equal(jacobian.shape, [random_ydata.shape[1], random_xdata.shape[1], random_xdata.shape[0]])
 
+        # StarNet2017
+        starnet2017 = StarNet2017()
+        starnet2017.max_epochs = 1
+        starnet2017.train(random_xdata, random_ydata)
+        prediction = starnet2017.test(random_xdata)
+        jacobian = neuralnet.jacobian(random_xdata)
+
+        np.testing.assert_array_equal(prediction.shape, random_ydata.shape)
+        np.testing.assert_array_equal(jacobian.shape, [random_ydata.shape[1], random_xdata.shape[1], random_xdata.shape[0]])
 
 if __name__ == '__main__':
     unittest.main()
