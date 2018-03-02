fix bayesianDropout logic, fix jacobian shape issue for colored images

astroNN/models/NeuralNetMaster.py
@@ -244,7 +244,7 @@ class NeuralNetMaster(ABC):
                 monoflag = True
                 x_data = x_data[:, :, :, np.newaxis]
 
-            jacobian = np.ones((self.labels_shape, x_data.shape[2], x_data.shape[1], x_data.shape[3], x_data.shape[0]),
+            jacobian = np.ones((self.labels_shape, x_data.shape[1], x_data.shape[2], x_data.shape[3], x_data.shape[0]),
                                dtype=np.float32)
 
             grad_list = []

astroNN/nn/utilities/custom_layers.py
@@ -58,9 +58,9 @@ class BayesianDropout(Layer):
     def call(self, inputs, training=None):
         retain_prob = 1. - self.rate
         if self.disable_layer is True:
-            return tf.nn.dropout(inputs * 1., retain_prob)
+            return input
         else:
-            return inputs
+            return tf.nn.dropout(inputs * 1., retain_prob)
 
     def get_config(self):
         config = {'rate': self.rate}