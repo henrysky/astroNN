e1725a556c46b6f8375239391f8c24fb734705fe
Henry Leung <henryskyleung@gmail.com>
2018-03-08 12:58:37 -0500

    add SpatialDropout 1D and 2D test case

--- test/layers_test.py
+++ test/layers_test.py
@@ -2,13 +2,18 @@ import unittest
 import numpy as np
 import numpy.testing as npt
 
-
+from astroNN.shared.nn_tools import gpu_memory_manage
 from astroNN import keras_import_manager
 keras = keras_import_manager()
 
 Input = keras.layers.Input
 Dense = keras.layers.Dense
+Conv1D = keras.layers.Conv1D
+Conv2D = keras.layers.Conv2D
+Flatten = keras.layers.Flatten
 Model = keras.models.Model
+
+gpu_memory_manage()
 
 
 class LayerCase(unittest.TestCase):
@@ -17,8 +22,8 @@ class LayerCase(unittest.TestCase):
         from astroNN.nn.layers import BayesianDropout
 
         # Data preparation
-        random_xdata = np.random.normal(0, 1, (1000, 7514))
-        random_ydata = np.random.normal(0, 1, (1000, 25))
+        random_xdata = np.random.normal(0, 1, (100, 7514))
+        random_ydata = np.random.normal(0, 1, (100, 25))
 
         input = Input(shape=[7514])
         dense = Dense(100)(input)
@@ -39,12 +44,58 @@ class LayerCase(unittest.TestCase):
         from astroNN.nn.layers import ConcreteDropout
 
         # Data preparation
-        random_xdata = np.random.normal(0, 1, (1000, 7514))
-        random_ydata = np.random.normal(0, 1, (1000, 25))
+        random_xdata = np.random.normal(0, 1, (100, 7514))
+        random_ydata = np.random.normal(0, 1, (100, 25))
 
         input = Input(shape=[7514])
         dense = ConcreteDropout(Dense(100))(input)
         output = Dense(25)(dense)
+        model = Model(inputs=input, outputs=output)
+        model.compile(optimizer='sgd', loss='mse')
+
+        model.fit(random_xdata, random_ydata, batch_size=128)
+
+        # make sure dropout is on even in testing phase
+        x = model.predict(random_xdata)
+        y = model.predict(random_xdata)
+        npt.assert_equal(np.any(np.not_equal(x, y)), True)
+
+    def test_SpatialDropout1D(self):
+        print('==========SpatialDropout1D test==========')
+        from astroNN.nn.layers import BayesianSpatialDropout1D
+
+        # Data preparation
+        random_xdata = np.random.normal(0, 1, (100, 7514, 1))
+        random_ydata = np.random.normal(0, 1, (100, 25))
+
+        input = Input(shape=[7514, 1])
+        conv = Conv1D(kernel_initializer='he_normal', padding="same", filters=2, kernel_size=16)(input)
+        dropout = BayesianSpatialDropout1D(0.2)(conv)
+        flattened = Flatten()(dropout)
+        output = Dense(25)(flattened)
+        model = Model(inputs=input, outputs=output)
+        model.compile(optimizer='sgd', loss='mse')
+
+        model.fit(random_xdata, random_ydata, batch_size=128)
+
+        # make sure dropout is on even in testing phase
+        x = model.predict(random_xdata)
+        y = model.predict(random_xdata)
+        npt.assert_equal(np.any(np.not_equal(x, y)), True)
+
+    def test_SpatialDropout12D(self):
+        print('==========SpatialDropout2D test==========')
+        from astroNN.nn.layers import BayesianSpatialDropout2D
+
+        # Data preparation
+        random_xdata = np.random.normal(0, 1, (100, 28, 28, 1))
+        random_ydata = np.random.normal(0, 1, (100, 25))
+
+        input = Input(shape=[28, 28, 1])
+        conv = Conv2D(kernel_initializer='he_normal', padding="same", filters=2, kernel_size=16)(input)
+        dropout = BayesianSpatialDropout2D(0.2)(conv)
+        flattened = Flatten()(dropout)
+        output = Dense(25)(flattened)
         model = Model(inputs=input, outputs=output)
         model.compile(optimizer='sgd', loss='mse')
 
