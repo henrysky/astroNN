308c5fdaa17c434917600cd2b7d68711e044d555
Henry Leung <henryskyleung@gmail.com>
2018-03-28 13:43:21 -0400

    test docs [ci skip]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,8 @@ import os
 #
 # needs_sphinx = '1.0'
 
+sys.path.insert(0, os.path.abspath('../astroNN/'))
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
