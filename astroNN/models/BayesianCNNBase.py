faster data normalization, normalization related bug fix
temp

astroNN/models/BayesianCNNBase.py
@@ -139,7 +139,7 @@ class BayesianCNNBase(NeuralNetMaster, ABC):
         self.inv_model_precision = None  # inverse model precision
         self.dropout_rate = 0.2
         self.length_scale = 3  # prior length scale
-        self.mc_num = 20
+        self.mc_num = 100  # increased to 100 due to high performance VI on GPU implemented on 14 April 2018 (Henry)
         self.val_size = 0.1
         self.disable_dropout = False
 
@@ -172,9 +172,9 @@ class BayesianCNNBase(NeuralNetMaster, ABC):
 
         # if no error array then just zeros
         if inputs_err is None:
-            inputs_err = np.atleast_2d(inputs_err)
             inputs_err = np.zeros_like(input_data)
         else:
+            inputs_err = np.atleast_2d(inputs_err)
             inputs_err /= self.input_std
 
         total_test_num = input_data.shape[0]  # Number of testing data
@@ -428,8 +428,8 @@ class BayesianCNNBase(NeuralNetMaster, ABC):
             norm_labels = self.labels_normalizer.normalize(labels)
             self.labels_mean, self.labels_std = self.labels_normalizer.mean_labels, self.labels_normalizer.std_labels
         else:
-            norm_data = self.input_normalizer.denormalize(input_data)
-            norm_labels = self.labels_normalizer.denormalize(labels)
+            norm_data = self.input_normalizer.normalize(input_data, calc=False)
+            norm_labels = self.labels_normalizer.normalize(labels, calc=False)
 
         # No need to care about Magic number as loss function looks for magic num in y_true only
         norm_input_err = input_err / self.input_std
@@ -451,7 +451,7 @@ class BayesianCNNBase(NeuralNetMaster, ABC):
                                                                                        norm_input_err[test_idx],
                                                                                        norm_labels_err[test_idx])
 
-        return input_data, labels, labels_err
+        return norm_data, norm_labels, norm_labels_err
 
     def post_training_checklist_child(self):
         astronn_model = 'model_weights.h5'

astroNN/nn/numpy.py
@@ -79,7 +79,7 @@ def relu(x):
     return x * (x > 0)
 
 
-def mean_absolute_percentage_error(x, y):
+def mean_absolute_percentage_error(x, y, axis=None):
     """
     NAME: mean_absolute_percentage_error
     PURPOSE:
@@ -88,6 +88,7 @@ def mean_absolute_percentage_error(x, y):
     INPUT:
         x (ndarray, astropy quantity): prediction
         y (ndarray, astropy quantity): ground truth
+        axis (int): numpy axis
     OUTPUT:
         (ndarray) representing activated ndarray
     HISTORY:
@@ -98,4 +99,4 @@ def mean_absolute_percentage_error(x, y):
     if type(y) == u.quantity.Quantity:
         y = y.value
 
-    return np.mean((np.abs(x - y) / y)[(x != MAGIC_NUMBER) & (y != MAGIC_NUMBER)])
+    return np.ma.array(np.abs((x-y)/y)*100., mask=[(x == MAGIC_NUMBER) | (y == MAGIC_NUMBER)]).mean(axis=axis)

astroNN/nn/utilities/normalizer.py
@@ -34,7 +34,13 @@ class Normalizer(object):
         self._custom_norm_func = None
         self._custom_denorm_func = None
 
-    def mode_checker(self):
+    def mode_checker(self, data):
+
+        if data.ndim == 1:
+            data_array = np.expand_dims(data, 1)
+        else:
+            data_array = np.array(data)
+
         self.normalization_mode = str(self.normalization_mode)  # just to prevent unnecessary type issue
         if self.normalization_mode == '0':
             self.featurewise_center = False
@@ -76,15 +82,12 @@ class Normalizer(object):
         else:
             raise ValueError(f"Unknown Mode -> {self.normalization_mode}")
 
+        return data_array
+
     def normalize(self, data, calc=True):
-        self.mode_checker()
+        data_array = self.mode_checker(data)
 
-        if data.ndim == 1:
-            data_array = np.expand_dims(data, 1)
-        else:
-            data_array = np.array(data)
-
-        magic_mask = [data_array == MAGIC_NUMBER]
+        magic_mask = [(data_array == MAGIC_NUMBER)]
 
         if calc is True:
             print(f'====Message from {self.__class__.__name__}====')
@@ -102,9 +105,12 @@ class Normalizer(object):
                     self.mean_labels[i] = np.mean((data_array[:, i])[not9999_index], axis=0)
                     (data_array[:, i])[not9999_index] -= self.mean_labels[i]
 
+                # self.mean_labels = np.ma.array(data_array, mask=[(data_array == MAGIC_NUMBER)]).mean(axis=0)
+                # data_array -= self.mean_labels
+
             if self.datasetwise_center is True:
                 self.mean_labels = np.mean(data_array[(data_array != MAGIC_NUMBER)])
-                data_array[(data_array != MAGIC_NUMBER)] -= self.mean_labels
+                data_array -= self.mean_labels
 
             if self.featurewise_stdalization is True:
                 self.std_labels = np.ones(data_array.shape[1])
@@ -113,9 +119,12 @@ class Normalizer(object):
                     self.std_labels[i] = np.std((data_array[:, i])[not9999_index], axis=0)
                     (data_array[:, i])[not9999_index] /= self.std_labels[i]
 
+                # self.std_labels = np.ma.array(data_array, mask=[(data_array == MAGIC_NUMBER)]).std(axis=0)
+                # data_array /= self.std_labels
+
             if self.datasetwise_stdalization is True:
                 self.std_labels = np.std(data_array[(data_array != MAGIC_NUMBER)])
-                data_array[(data_array != MAGIC_NUMBER)] /= self.std_labels
+                data_array /= self.std_labels
 
             if self.normalization_mode == 255:
                 data_array -= self.mean_labels
@@ -132,12 +141,7 @@ class Normalizer(object):
         return data_array
 
     def denormalize(self, data):
-        self.mode_checker()
-
-        if data.ndim == 1:
-            data_array = np.expand_dims(data, 1)
-        else:
-            data_array = np.array(data)
+        data_array = self.mode_checker(data)
 
         magic_mask = [data_array == MAGIC_NUMBER]
 