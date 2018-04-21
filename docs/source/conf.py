bb11d45d0dedb9d3a7c1071967ad0983ce51426f
Henry Leung <henryskyleung@gmail.com>
2018-04-20 21:37:56 -0400

    test rtd again [ci skip]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,13 +27,14 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
+
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
         return MagicMock()
 
 
-MOCK_MODULES = ['numpy', 'scipy', 'scipy.linalg', 'scipy.signal', 'tensorflow', 'h5py', 'keras', 'pandas', 'h5py', 'pandas', 'scikit-learn']
+MOCK_MODULES = ['numpy', 'scipy', 'tensorflow', 'h5py', 'keras', 'pandas', 'h5py', 'pandas', 'scikit-learn']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
@@ -49,7 +50,8 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
-    'sphinx.ext.mathjax']
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
