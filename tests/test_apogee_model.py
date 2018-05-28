d7bc4b6b6b451ab136228553b0481a9c62542477
henrysky <henryskyleung@gmail.com>
2018-05-28 16:01:19 -0400

    make travix CI test faster

--- tests/test_apogee_model.py
+++ tests/test_apogee_model.py
@@ -119,21 +119,6 @@ class ApogeeModelTestCase(unittest.TestCase):
         np.testing.assert_array_equal(prediction.shape, random_ydata.shape)
         bneuralnetcensored.save(name='apogee_bcnncensored')
 
-        # just to make sure it can load it back without error
-        bneuralnetcensored_loaded = load_folder("apogee_bcnncensored")
-        bneuralnetcensored_loaded.callbacks = ErrorOnNaN()
-
-        # prevent memory issue on Tavis CI
-        bneuralnetcensored_loaded.mc_num = 3
-        pred, pred_err = bneuralnetcensored_loaded.test(random_xdata)
-        bneuralnetcensored_loaded.save()
-
-        # Fine-tuning test
-        bneuralnetcensored_loaded.max_epochs = 1
-        bneuralnetcensored_loaded.train(random_xdata, random_ydata)
-
-        pred, pred_err = bneuralnetcensored_loaded.test_old(random_xdata)
-
     def test_apogee_cvae(self):
         # Data preparation, keep the data size large (>800 data points to prevent issues)
         random_xdata = np.random.normal(0, 1, (1000, 7514))
