ac3151b8a73b624179f792a8906ec67b64a2a58a
henrysky <henryskyleung@gmail.com>
2018-08-25 10:49:33 -0400

    use min_delta instead of epsilon and test it

--- astroNN/models/CNNBase.py
+++ astroNN/models/CNNBase.py
@@ -217,15 +217,10 @@ class CNNBase(NeuralNetMaster, ABC):
         # Call the checklist to create astroNN folder and save parameters
         self.pre_training_checklist_child(input_data, labels)
 
-        try:
-            reduce_lr = ReduceLROnPlateau(monitor='val_mean_absolute_error', factor=0.5,
-                                          min_delta=self.reduce_lr_epsilon,
-                                          patience=self.reduce_lr_patience, min_lr=self.reduce_lr_min, mode='min',
-                                          verbose=2)
-        except TypeError:
-            reduce_lr = ReduceLROnPlateau(monitor='val_mean_absolute_error', factor=0.5, epsilon=self.reduce_lr_epsilon,
-                                          patience=self.reduce_lr_patience, min_lr=self.reduce_lr_min, mode='min',
-                                          verbose=2)
+        reduce_lr = ReduceLROnPlateau(monitor='val_mean_absolute_error', factor=0.5,
+                                      min_delta=self.reduce_lr_epsilon,
+                                      patience=self.reduce_lr_patience, min_lr=self.reduce_lr_min, mode='min',
+                                      verbose=2)
 
         early_stopping = EarlyStopping(monitor='val_mean_absolute_error', min_delta=self.early_stopping_min_delta,
                                        patience=self.early_stopping_patience, verbose=2, mode='min')
