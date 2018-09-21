34ef87264b9be371590279277ff8e20be8524482
henrysky <henryskyleung@gmail.com>
2018-09-20 21:51:04 -0400

    update simplepoly arguement

--- astroNN/models/SimplePolyNN.py
+++ astroNN/models/SimplePolyNN.py
@@ -18,7 +18,7 @@ class SimplePolyNN(CNNBase):
     :History: 2018-Jul-23 - Written - Henry Leung (University of Toronto)
     """
 
-    def __init__(self, lr=0.005, init_w=None):
+    def __init__(self, lr=0.005, init_w=None, use_xbias=False):
         super().__init__()
 
         self._implementation_version = '1.0'
@@ -33,14 +33,18 @@ class SimplePolyNN(CNNBase):
         self.input_norm_mode = 0
         self.labels_norm_mode = 0
         self.init_w = init_w
-
+        self.use_xbias = use_xbias
         self.task = 'regression'
         self.targetname = ['unbiased_parallax']
 
     def model(self):
         input_tensor = Input(shape=self._input_shape, name='input')
         flattener = Flatten()(input_tensor)
-        output = PolyFit(deg=self.num_hidden, use_xbias=True, name='output', init_w=self.init_w,
+        output = PolyFit(deg=self.num_hidden,
+                         output_units=self._labels_shape,
+                         use_xbias=self.use_xbias,
+                         name='output',
+                         init_w=self.init_w,
                          kernel_regularizer=regularizers.l2(self.l2))(flattener)
 
         model = Model(inputs=input_tensor, outputs=output)
