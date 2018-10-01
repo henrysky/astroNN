759096d3eb77df031c1d18410b94cac10d3f1ee9
henrysky <henryskyleung@gmail.com>
2018-10-01 19:00:35 -0400

    update test cases

--- tests/test_loss_func.py
+++ tests/test_loss_func.py
@@ -20,23 +20,25 @@ keras.backend.set_session(sess)
 
 
 class LossFuncTestCase(unittest.TestCase):
-    def test_loss_func(self):
+    def test_loss_func_util(self):
         # make sure custom reduce_var works
         content = [1, 2, 3, 4, 5]
-        var_array = tf.Variable(content)
-        sess.run(tf.global_variables_initializer())
+        var_array = tf.constant(content)
+        
         self.assertEqual(reduce_var(var_array).eval(session=get_session()), np.var(content))
 
+    def test_loss_magic(self):
         # =============Magic correction term============= #
-        y_true = tf.Variable([[2., MAGIC_NUMBER, MAGIC_NUMBER], [2., MAGIC_NUMBER, 4.]])
-        sess.run(tf.global_variables_initializer())
+        y_true = tf.constant([[2., MAGIC_NUMBER, MAGIC_NUMBER], [2., MAGIC_NUMBER, 4.]])
+
         npt.assert_array_equal(magic_correction_term(y_true).eval(session=get_session()), [3., 1.5])
 
+    def test_loss_mse(self):
         # =============MSE/MAE============= #
-        y_pred = tf.Variable([[2., 3., 4.], [2., 3., 7.]])
-        y_pred_2 = tf.Variable([[2., 9., 4.], [2., 0., 7.]])
-        y_true = tf.Variable([[2., MAGIC_NUMBER, 4.], [2., MAGIC_NUMBER, 4.]])
-        sess.run(tf.global_variables_initializer())
+        y_pred = tf.constant([[2., 3., 4.], [2., 3., 7.]])
+        y_pred_2 = tf.constant([[2., 9., 4.], [2., 0., 7.]])
+        y_true = tf.constant([[2., MAGIC_NUMBER, 4.], [2., MAGIC_NUMBER, 4.]])
+        
         npt.assert_almost_equal(mean_absolute_error(y_true, y_pred).eval(session=get_session()), [0., 3. / 2.])
         npt.assert_almost_equal(mean_squared_error(y_true, y_pred).eval(session=get_session()), [0., 9. / 2])
 
@@ -46,25 +48,28 @@ class LossFuncTestCase(unittest.TestCase):
         npt.assert_almost_equal(mean_squared_error(y_true, y_pred).eval(session=get_session()),
                                 mean_squared_error(y_true, y_pred_2).eval(session=get_session()))
 
+    def test_loss_mean_err(self):
         # =============Mean Error============= #
-        y_pred = tf.Variable([[1., 3., 4.], [2., 3., 7.]])
-        y_true = tf.Variable([[2., MAGIC_NUMBER, 3.], [2., MAGIC_NUMBER, 7.]])
-        sess.run(tf.global_variables_initializer())
+        y_pred = tf.constant([[1., 3., 4.], [2., 3., 7.]])
+        y_true = tf.constant([[2., MAGIC_NUMBER, 3.], [2., MAGIC_NUMBER, 7.]])
+        
         npt.assert_almost_equal(mean_error(y_true, y_pred).eval(session=get_session()), [0., 0.])
 
+    def test_loss_acurrancy(self):
         # =============Accuracy============= #
-        y_pred = tf.Variable([[1., 0., 0.], [1., 0., 0.]])
-        y_true = tf.Variable([[1., MAGIC_NUMBER, 1.], [0., MAGIC_NUMBER, 1.]])
-        sess.run(tf.global_variables_initializer())
+        y_pred = tf.constant([[1., 0., 0.], [1., 0., 0.]])
+        y_true = tf.constant([[1., MAGIC_NUMBER, 1.], [0., MAGIC_NUMBER, 1.]])
+        
         npt.assert_array_equal(categorical_accuracy(y_true, y_pred).eval(session=get_session()), [1., 0.])
         npt.assert_almost_equal(binary_accuracy(from_logits=False)(y_true, y_pred).eval(session=get_session()),
                                 [1. / 2., 0.])
 
+    def test_loss_abs_error(self):
         # =============Abs Percentage Accuracy============= #
-        y_pred = tf.Variable([[1., 0., 0.], [1., 0., 0.]])
-        y_pred_2 = tf.Variable([[1., 9., 0.], [1., -1., 0.]])
-        y_true = tf.Variable([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 1.]])
-        sess.run(tf.global_variables_initializer())
+        y_pred = tf.constant([[1., 0., 0.], [1., 0., 0.]])
+        y_pred_2 = tf.constant([[1., 9., 0.], [1., -1., 0.]])
+        y_true = tf.constant([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 1.]])
+        
 
         npt.assert_array_almost_equal(mean_absolute_percentage_error(y_true, y_pred).eval(session=get_session()),
                                       [50., 50.], decimal=3)
@@ -73,11 +78,12 @@ class LossFuncTestCase(unittest.TestCase):
                                       mean_absolute_percentage_error(y_true, y_pred_2).eval(session=get_session()),
                                       decimal=3)
 
+    def test_loss_percentage_error(self):
         # =============Percentage Accuracy============= #
-        y_pred = tf.Variable([[1., 0., 0.], [1., 0., 0.]])
-        y_pred_2 = tf.Variable([[1., 9., 0.], [1., -1., 0.]])
-        y_true = tf.Variable([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 1.]])
-        sess.run(tf.global_variables_initializer())
+        y_pred = tf.constant([[1., 0., 0.], [1., 0., 0.]])
+        y_pred_2 = tf.constant([[1., 9., 0.], [1., -1., 0.]])
+        y_true = tf.constant([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 1.]])
+        
 
         npt.assert_array_almost_equal(mean_percentage_error(y_true, y_pred).eval(session=get_session()),
                                       [50., 50.], decimal=3)
@@ -86,11 +92,12 @@ class LossFuncTestCase(unittest.TestCase):
                                       mean_percentage_error(y_true, y_pred_2).eval(session=get_session()),
                                       decimal=3)
 
+    def test_loss_log_error(self):
         # =============Mean Squared Log Error============= #
-        y_pred = tf.Variable([[1., 0., 0.], [1., 0., 0.]])
-        y_pred_2 = tf.Variable([[1., 9., 0.], [1., -1., 0.]])
-        y_true = tf.Variable([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 1.]])
-        sess.run(tf.global_variables_initializer())
+        y_pred = tf.constant([[1., 0., 0.], [1., 0., 0.]])
+        y_pred_2 = tf.constant([[1., 9., 0.], [1., -1., 0.]])
+        y_true = tf.constant([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 1.]])
+        
         npt.assert_array_almost_equal(mean_squared_logarithmic_error(y_true, y_pred).eval(session=get_session()),
                                       [0.24, 0.24], decimal=3)
         # make sure neural network prediction won't matter for magic number term
@@ -98,16 +105,17 @@ class LossFuncTestCase(unittest.TestCase):
                                       mean_squared_logarithmic_error(y_true, y_pred_2).eval(session=get_session()),
                                       decimal=3)
 
+    def test_loss_zeros(self):
         # =============Zeros Loss============= #
-        y_pred = tf.Variable([[1., 0., 0.], [5., -9., 2.]])
-        y_true = tf.Variable([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 1.]])
-        sess.run(tf.global_variables_initializer())
+        y_pred = tf.constant([[1., 0., 0.], [5., -9., 2.]])
+        y_true = tf.constant([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 1.]])
+        
         npt.assert_array_almost_equal(zeros_loss(y_true, y_pred).eval(session=get_session()), [0., 0.])
 
     def test_categorical_crossentropy(self):
-        y_pred = tf.Variable([[1., 0., 1.], [2., 1., 0.]])
-        y_true = tf.Variable([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 0.]])
-        sess.run(tf.global_variables_initializer())
+        y_pred = tf.constant([[1., 0., 1.], [2., 1., 0.]])
+        y_true = tf.constant([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 0.]])
+        
         y_pred_softmax = tf.nn.softmax(y_pred)
         # Truth with Magic number is wrong
         npt.assert_array_almost_equal(categorical_crossentropy(y_true, y_pred_softmax).eval(session=get_session()),
@@ -115,13 +123,13 @@ class LossFuncTestCase(unittest.TestCase):
                                           session=get_session()), decimal=3)
 
     def test_binary_crossentropy(self):
-        y_pred = tf.Variable([[0.5, 0., 1.], [2., 0., -1.]])
-        y_pred_2 = tf.Variable([[0.5, 2., 1.], [2., 2., -1.]])
-        y_true = tf.Variable([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 0.]])
+        y_pred = tf.constant([[0.5, 0., 1.], [2., 0., -1.]])
+        y_pred_2 = tf.constant([[0.5, 2., 1.], [2., 2., -1.]])
+        y_true = tf.constant([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 0.]])
         y_pred_sigmoid = tf.nn.sigmoid(y_pred)
         y_pred_2_sigmoid = tf.nn.sigmoid(y_pred_2)
         # Truth with Magic number is wrong
-        sess.run(tf.global_variables_initializer())
+        
         npt.assert_array_almost_equal(binary_crossentropy(y_true, y_pred_sigmoid).eval(session=get_session()),
                                       binary_crossentropy(y_true, y_pred, from_logits=True).eval(
                                           session=get_session()), decimal=3)
@@ -135,9 +143,9 @@ class LossFuncTestCase(unittest.TestCase):
                                           session=get_session()), decimal=3)
 
     def test_negative_log_likelihood(self):
-        y_pred = tf.Variable([[0.5, 0., 1.], [2., 0., -1.]])
-        y_true = tf.Variable([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 0.]])
-        sess.run(tf.global_variables_initializer())
+        y_pred = tf.constant([[0.5, 0., 1.], [2., 0., -1.]])
+        y_true = tf.constant([[1., MAGIC_NUMBER, 1.], [1., MAGIC_NUMBER, 0.]])
+        
         npt.assert_array_almost_equal(nll(y_true, y_pred).eval(session=get_session()), 0.34657377, decimal=3)
 
 
