fd909e732778abf863791aa70e90d35595101343
Henry Leung <henryskyleung@gmail.com>
2023-04-26 13:41:09 -0400

    fix versioning [ci skip]

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ python_min_version = "3.8"
 
 setup(
     name="astroNN",
-    version="1.1",
+    version="1.1.0",
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
