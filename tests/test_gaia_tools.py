0ae1565e8d926f3217832ea518e026d981d95952
Henry Leung <henryskyleung@gmail.com>
2024-07-19 17:21:06 -0400

    switch from unittest assertEqual to numpy testing

--- tests/test_gaia_tools.py
+++ tests/test_gaia_tools.py
@@ -21,7 +21,7 @@ class GaiaToolsCase(unittest.TestCase):
         pc = absmag_to_pc(absmag, mag)
         npt.assert_almost_equal(pc.value, 1000 / parallax, decimal=1)
         npt.assert_almost_equal(mag_to_absmag(mag, parallax * u.mas), absmag, decimal=1)
-        self.assertEqual(np.any(absmag_to_fakemag(MAGIC_NUMBER) == MAGIC_NUMBER), True)
+        npt.assert_equal(absmag_to_fakemag(MAGIC_NUMBER), MAGIC_NUMBER)
         absmag_test, absmag_err_test = mag_to_absmag(mag, parallax * u.mas, parallax_err)
         absmag_test_uniterr, absmag_err_test_uniterr = mag_to_absmag(mag, parallax * u.mas,
                                                                      parallax_err / 1000 * u.arcsec)
@@ -75,51 +75,51 @@ class GaiaToolsCase(unittest.TestCase):
 
         # check gaia default dr
         dr = gaia_default_dr()
-        self.assertEqual(dr, 2)
+        npt.assert_equal(dr, 2)
         dr = gaia_default_dr(dr=3)
-        self.assertEqual(dr, 3)
+        npt.assert_equal(dr, 3)
 
     def test_logsol(self):
         # Test conversion tools related to log solar luminosity
         from astroNN.gaia import fakemag_to_logsol, absmag_to_logsol, logsol_to_absmag, logsol_to_fakemag
-        self.assertEqual(logsol_to_fakemag(fakemag_to_logsol(100.)), 100.)
+        npt.assert_equal(logsol_to_fakemag(fakemag_to_logsol(100.)), 100.)
         npt.assert_array_equal(logsol_to_fakemag(fakemag_to_logsol([100., 100.])), [100., 100.])
         npt.assert_array_equal(logsol_to_fakemag(fakemag_to_logsol(np.array([100, 100, 100]))), [100., 100., 100.])
-        self.assertEqual(fakemag_to_logsol(MAGIC_NUMBER), MAGIC_NUMBER)
-        self.assertEqual(logsol_to_fakemag(fakemag_to_logsol(MAGIC_NUMBER)), MAGIC_NUMBER)
-        self.assertEqual(np.any(fakemag_to_logsol([MAGIC_NUMBER, 1000]) == MAGIC_NUMBER), True)
+        npt.assert_equal(fakemag_to_logsol(MAGIC_NUMBER), MAGIC_NUMBER)
+        npt.assert_equal(logsol_to_fakemag(fakemag_to_logsol(MAGIC_NUMBER)), MAGIC_NUMBER)
+        npt.assert_equal(np.any(fakemag_to_logsol([MAGIC_NUMBER, 1000]) == MAGIC_NUMBER), True)
 
-        self.assertEqual(logsol_to_absmag(absmag_to_logsol(99.)), 99.)
+        npt.assert_equal(logsol_to_absmag(absmag_to_logsol(99.)), 99.)
         self.assertAlmostEqual(logsol_to_absmag(absmag_to_logsol(-99.)), -99.)
         npt.assert_array_equal(logsol_to_absmag(absmag_to_logsol([99., 99.])), [99., 99.])
         npt.assert_array_almost_equal(logsol_to_absmag(absmag_to_logsol([-99., -99.])), [-99., -99.])
         npt.assert_array_almost_equal(logsol_to_absmag(absmag_to_logsol(np.array([99., 99., 99.]))), [99., 99., 99.])
-        self.assertEqual(absmag_to_logsol(MAGIC_NUMBER), MAGIC_NUMBER)
-        self.assertEqual(logsol_to_absmag(absmag_to_logsol(MAGIC_NUMBER)), MAGIC_NUMBER)
-        self.assertEqual(np.any(absmag_to_logsol([MAGIC_NUMBER, 1000]) == MAGIC_NUMBER), True)
+        npt.assert_equal(absmag_to_logsol(MAGIC_NUMBER), MAGIC_NUMBER)
+        npt.assert_equal(logsol_to_absmag(absmag_to_logsol(MAGIC_NUMBER)), MAGIC_NUMBER)
+        npt.assert_equal(np.any(absmag_to_logsol([MAGIC_NUMBER, 1000]) == MAGIC_NUMBER), True)
 
     def test_extinction(self):
         from astroNN.gaia import extinction_correction
 
-        self.assertEqual(np.any([extinction_correction(10., -90.) == -9999.]), False)
-        self.assertEqual(extinction_correction(10., -90.), 10.)
-        self.assertEqual(np.any([extinction_correction(-99.99, -90.) == -9999.]), True)
+        npt.assert_equal(np.any([extinction_correction(10., -90.) == -9999.]), False)
+        npt.assert_equal(extinction_correction(10., -90.), 10.)
+        npt.assert_equal(np.any([extinction_correction(-99.99, -90.) == -9999.]), True)
 
     def test_known_regression(self):
         # prevent regression of known bug
-        self.assertEqual(mag_to_absmag(1., MAGIC_NUMBER), MAGIC_NUMBER)
-        self.assertEqual(mag_to_absmag(MAGIC_NUMBER, MAGIC_NUMBER), MAGIC_NUMBER)
-        self.assertEqual(
+        npt.assert_equal(mag_to_absmag(1., MAGIC_NUMBER), MAGIC_NUMBER)
+        npt.assert_equal(mag_to_absmag(MAGIC_NUMBER, MAGIC_NUMBER), MAGIC_NUMBER)
+        npt.assert_equal(
             np.all(mag_to_absmag(MAGIC_NUMBER, MAGIC_NUMBER, 1.) == (MAGIC_NUMBER, MAGIC_NUMBER)), True)
-        self.assertEqual(
+        npt.assert_equal(
             np.all(mag_to_fakemag(MAGIC_NUMBER, MAGIC_NUMBER, 1.) == (MAGIC_NUMBER, MAGIC_NUMBER)), True)
 
-        self.assertEqual(mag_to_fakemag(1., MAGIC_NUMBER), MAGIC_NUMBER)
-        self.assertEqual(mag_to_fakemag(MAGIC_NUMBER, MAGIC_NUMBER), MAGIC_NUMBER)
+        npt.assert_equal(mag_to_fakemag(1., MAGIC_NUMBER), MAGIC_NUMBER)
+        npt.assert_equal(mag_to_fakemag(MAGIC_NUMBER, MAGIC_NUMBER), MAGIC_NUMBER)
 
-        self.assertEqual(fakemag_to_pc(1., MAGIC_NUMBER).value, MAGIC_NUMBER)
-        self.assertEqual(fakemag_to_pc(-1., 2.).value, MAGIC_NUMBER)
-        self.assertEqual(absmag_to_pc(1., MAGIC_NUMBER).value, MAGIC_NUMBER)
+        npt.assert_equal(fakemag_to_pc(1., MAGIC_NUMBER).value, MAGIC_NUMBER)
+        npt.assert_equal(fakemag_to_pc(-1., 2.).value, MAGIC_NUMBER)
+        npt.assert_equal(absmag_to_pc(1., MAGIC_NUMBER).value, MAGIC_NUMBER)
 
     def test_anderson(self):
         from astroNN.gaia import anderson_2017_parallax
@@ -127,7 +127,7 @@ class GaiaToolsCase(unittest.TestCase):
         # Both parallax and para_var is in mas
         # cuts=True to cut bad data (negative parallax and percentage error more than 20%)
         ra, dec, parallax, para_err = anderson_2017_parallax(cuts=True)
-        self.assertEqual(np.any([parallax == -9999.]), False)  # assert no -9999
+        npt.assert_equal(np.any([parallax == -9999.]), False)  # assert no -9999
 
     def test_dr2_parallax(self):
         from astroNN.gaia import gaiadr2_parallax
@@ -138,16 +138,16 @@ class GaiaToolsCase(unittest.TestCase):
         ra02, dec02, parallax02, para_err02 = gaiadr2_parallax(cuts=0.2)
         ra01, dec01, parallax01, para_err01 = gaiadr2_parallax(cuts=0.1)
         # assert no -9999.
-        self.assertEqual(np.any([parallax == -9999.]), False)
+        npt.assert_equal(np.any([parallax == -9999.]), False)
         # assert cuts = True equals 0.2
-        self.assertEqual(np.any([ra == ra02]), True)
-        self.assertEqual((ra01.shape[0] < ra02.shape[0]), True)
+        npt.assert_equal(np.any([ra == ra02]), True)
+        npt.assert_equal((ra01.shape[0] < ra02.shape[0]), True)
         # assert no ridiculous parallax if do cut
-        self.assertEqual(np.any([((para_err / parallax) > 0.2) & (parallax < 0.)]), False)
+        npt.assert_equal(np.any([((para_err / parallax) > 0.2) & (parallax < 0.)]), False)
 
         ra, dec, parallax, para_err = gaiadr2_parallax(cuts=False)
         # assert some -9999. due to no cuts
-        self.assertEqual(np.any([parallax == -9999.]), True)
+        npt.assert_equal(np.any([parallax == -9999.]), True)
         ra, dec, parallax, para_err = gaiadr2_parallax(cuts=True, keepdims=True)
 
 
