fix jacobian and hessian function again

src/astroNN/models/base_master_nn.py
@@ -507,6 +507,7 @@ class NeuralNetMaster(ABC):
         if input_dim > 3 or output_dim > 3:
             raise ValueError("Unsupported data dimension")
 
+        start_time = time.time()
         if keras.backend.backend() == "tensorflow":
             import tensorflow as tf
 
@@ -519,19 +520,19 @@ class NeuralNetMaster(ABC):
                     temp = _model(xtensor)
                 jacobian = tf.squeeze(dtape.batch_jacobian(temp, xtensor))
 
-            start_time = time.time()
-
             hessian = tf.squeeze(tape.batch_jacobian(jacobian, xtensor))
         elif keras.backend.backend() == "torch":
             import torch
 
-            xtensor = torch.tensor(x_data, requires_grad=True)
-            hessian = torch.vmap(torch.func.hessian(_model))(xtensor)
+            # add new axis for vmap
+            xtensor = torch.tensor(x_data, requires_grad=True)[:, None, ...]
+            hessian = torch.vmap(torch.func.hessian(_model), randomness="different")(xtensor)
+            hessian = torch.squeeze(hessian)
         else:
             raise ValueError("Only Tensorflow and PyTorch backend is supported")
-
+        
         if np.all(
-            hessian == 0.0
+            keras.ops.convert_to_numpy(keras.ops.equal(hessian, 0.0))
         ):  # warn user about not so linear activation like ReLU will get all zeros
             warnings.warn(
                 "The hessians is detected to be all zeros. The common cause is you did not use any activation or "
@@ -648,8 +649,10 @@ class NeuralNetMaster(ABC):
         elif keras.backend.backend() == "torch":
             import torch
 
-            xtensor = torch.tensor(x_data, requires_grad=True)
-            jacobian = torch.vmap(torch.func.jacrev(_model))(xtensor)
+            # add new axis for vmap
+            xtensor = torch.tensor(x_data, requires_grad=True)[:, None, ...]
+            jacobian = torch.vmap(torch.func.jacrev(_model), randomness="different")(xtensor)
+            jacobian = torch.squeeze(jacobian)
         else:
             raise ValueError("Only Tensorflow and PyTorch backend is supported")
 

tests/test_apogee_model.py
@@ -47,7 +47,7 @@ def test_apogee_cnn(spectra_ci_data):
 
     prediction = neuralnet.predict(xdata)
     # assert most of them have less than 15% error
-    assert 0.15 > np.median(mape(ydata[neuralnet.val_idx], prediction[neuralnet.val_idx])) / 100.
+    assert 0.15 > np.nanmedian(np.abs((ydata[neuralnet.val_idx] - prediction[neuralnet.val_idx]) / ydata[neuralnet.val_idx]))
     jacobian = neuralnet.jacobian(xdata[:5])
     # assert shape correct as expected
     npt.assert_array_equal(prediction.shape, ydata.shape)

tests/test_models.py
@@ -86,9 +86,8 @@ def test_color_images(mnist_data):
     mnist_test.save("cifar10_test")
     mnist_reloaded = load_folder("cifar10_test")
     prediction_loaded = mnist_reloaded.predict(x_test_color[:200])
-    # TODO: something is wrong here
-    # mnist_reloaded.jacobian(x_test[:2], mean_output=True, mc_num=2)
-    # mnist_reloaded.hessian_diag(x_test[:10], mean_output=True, mc_num=2)
+    mnist_reloaded.jacobian(x_test_color[:2], mean_output=True, mc_num=2)
+    mnist_reloaded.hessian(x_test_color[:2], mean_output=True, mc_num=2)
 
     # Cifar10_CNN is deterministic
     np.testing.assert_array_equal(prediction, prediction_loaded)
@@ -141,9 +140,8 @@ def test_bayesian_binary_mnist(mnist_data):
     net_reloaded.mc_num = 3
     prediction_loaded, prediction_loaded_err = net_reloaded.predict(x_test)
 
-    # TODO: something is wrong here
-    # assert (np.sum(np.argmax(pred, axis=1) == y_test)) / test_num > 0.9  # assert accuracy
-    # assert (np.sum(np.argmax(prediction_loaded, axis=1) == y_test)) / test_num > 0.9  # assert accuracy
+    # assert (np.sum(np.argmax(pred, axis=1) == np.argmax(y_test, axis=1))) / test_num > 0.9  # assert accuracy
+    # assert (np.sum(np.argmax(prediction_loaded, axis=1) == np.argmax(y_test, axis=1))) / test_num > 0.9  # assert accuracy
 
 
 def test_custom_model():