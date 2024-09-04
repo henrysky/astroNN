948e7a49cdd8e8ee644ca63e0ace8026137aa536
Henry Leung <henryskyleung@gmail.com>
2024-09-03 21:27:43 -0400

    fix intersphinx_mapping in docs config [ci skip]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -176,7 +176,7 @@ texinfo_documents = [
 ]
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/": None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 html_show_sourcelink = True
 
