658c87eaef9a138fc190bfac5c2bf3f8aa824842
Henry Leung <henryskyleung@gmail.com>
2024-10-26 00:35:11 -0400

    set min requirement to new tf 2.18 and torch 2.5 which support numpy 2.0

--- src/astroNN/gaia/__init__.py
+++ src/astroNN/gaia/__init__.py
@@ -14,3 +14,21 @@ from astroNN.gaia.gaia_shared import (
     fakemag_to_parallax,
     fakemag_to_mag,
 )
+
+__all__ = [
+    "gaia_default_dr",
+    "gaia_env",
+    "mag_to_absmag",
+    "mag_to_fakemag",
+    "absmag_to_pc",
+    "fakemag_to_absmag",
+    "absmag_to_fakemag",
+    "fakemag_to_pc",
+    "fakemag_to_logsol",
+    "absmag_to_logsol",
+    "logsol_to_fakemag",
+    "logsol_to_absmag",
+    "extinction_correction",
+    "fakemag_to_parallax",
+    "fakemag_to_mag",
+]
