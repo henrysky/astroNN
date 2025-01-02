2bccd6ba4846abd5125686ee7facf5fddf93717e
Henry Leung <henryskyleung@gmail.com>
2025-01-01 22:23:47 -0500

    bump to 2025

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,7 +68,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "astroNN"
-copyright = "2017-2024 Henry Leung"
+copyright = "2017-2025 Henry Leung"
 author = "Henry Leung"
 
 # The version info for the project you're documenting, acts as replacement for
